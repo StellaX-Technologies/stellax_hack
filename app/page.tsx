18959aea3444d53e796e509bfcfd4c489cd165f9
VVS Basanth Pedapati <vvs.pedapati@hotmail.com>
2024-09-04 17:42:48 +0530

    homePage

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
 import Hero from "@/components/hero";
-import ConnectSupabaseSteps from "@/components/tutorial/connect-supabase-steps";
-import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps";
 import { hasEnvVars } from "@/utils/supabase/check-env-vars";
 
 export default async function Index() {
@@ -8,8 +8,14 @@ export default async function Index() {
     <>
       <Hero />
       <main className="flex-1 flex flex-col gap-6 px-4">
-        <h2 className="font-medium text-xl mb-4">Next steps</h2>
-        {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
+        <Link
+          href="./"
+          target="_blank"
+        >
+          <Button className="flex items-center gap-2" size={"sm"}>
+            <span>Register Here</span>
+          </Button>
+        </Link>
       </main>
     </>
   );
